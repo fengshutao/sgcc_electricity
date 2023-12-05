48e198cf3200365462754cdb91e4365f7388244b
fengshutao <69944484+fengshutao@users.noreply.github.com>
2023-12-05 10:08:45 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,88 +1,119 @@
 import logging
-import logging.config
-import os
-import sys
+import pyautogui
+import pygetwindow as gw
+import random
 import time
-import traceback
 from datetime import datetime
 
-import dotenv
-import schedule
+import scraper.slider_image_process as sip
 
-from const import *
-from data_fetcher import DataFetcher
-from sensor_updator import SensorUpdator
+from playwright.sync_api import sync_playwright
 
 
-def main():
-    # 读取 .env 文件
-    dotenv.load_dotenv()
+class ElectricityScraper:
+    def __init__(self, config):
+        logging.info("初始化 ElectricityScraper")
+        logging.info("初始化 self.config")
+        self.config = config
+        logging.info("self.config 初始化完成")
+        logging.info("ElectricityScraper 初始化完成")
 
-    try:
-        PHONE_NUMBER = os.getenv("PHONE_NUMBER")
-        PASSWORD = os.getenv("PASSWORD")
-        HASS_URL = os.getenv("HASS_URL")
-        HASS_TOKEN = os.getenv("HASS_TOKEN")
-        JOB_START_TIME = os.getenv("JOB_START_TIME")
-        FIRST_SLEEP_TIME = int(os.getenv("FIRST_SLEEP_TIME"))
-        LOG_LEVEL = os.getenv("LOG_LEVEL")
+    def fetch_data(self):
+        with sync_playwright() as pw:
+            logging.info("尝试启动浏览器")
+            self.browser = pw.chromium.launch(headless=True)
+            logging.info("浏览器启动完成")
+            self.context = self.browser.new_context(viewport={'width': 1920, 'height': 1080})
+            self.page = self.context.new_page()
 
-    except Exception as e:
-        logging.error(f"读取.env文件失败，程序将退出，错误信息为{e}")
-        sys.exit()
+            self.page.goto("https://www.95598.cn/osgweb/login")
+            self.page.locator(".user").click()
+            self.page.get_by_placeholder("请输入用户名/手机号/邮箱").fill(self.config.get('Credentials', 'username'))
+            self.page.get_by_placeholder("请输入密码").fill(self.config.get('Credentials', 'password'))
 
-    logger_init(LOG_LEVEL)
-    logging.info("程序开始，当前仓库版本为1.3.3，仓库地址为https://github.com/renhai-lab/sgcc_electricity")
+            self.page.get_by_role("button", name="登录").click()
 
-    fetcher = DataFetcher(PHONE_NUMBER, PASSWORD)
-    updator = SensorUpdator(HASS_URL, HASS_TOKEN)
-    logging.info(f"当前登录的用户名为: {PHONE_NUMBER}，homeassistant地址为{HASS_URL},程序将在每天{JOB_START_TIME}执行")
-    schedule.every().day.at(JOB_START_TIME).do(run_task, fetcher, updator)
+            # 抓取验证码数据
+            self.loading_slide()
+            self.page.wait_for_selector('canvas')
+            slide_bg_img = self.page.evaluate("() => document.querySelector('canvas').toDataURL('image/png')")
+            slide_block_img = self.page.evaluate(
+                "() => document.querySelector('.slide-verify-block').toDataURL('image/png')")
 
-    if datetime.now().time() < datetime.strptime(JOB_START_TIME, "%H:%M").time():
-        logging.info(f"此次为首次运行，当前时间早于 JOB_START_TIME: {JOB_START_TIME}，{JOB_START_TIME}再执行！")
-        schedule.every().day.at(JOB_START_TIME).do(run_task, fetcher, updator)
-    else:
-        logging.info(f"此次为首次运行，等待时间(FIRST_SLEEP_TIME)为{FIRST_SLEEP_TIME}秒，可在.env中设置")
-        time.sleep(FIRST_SLEEP_TIME)
-        run_task(fetcher, updator)
+            slide_bg_img = sip.base64_to_img(slide_bg_img)
+            slide_block_img = sip.cutting_transparent_block(sip.base64_to_img(slide_block_img), offset=65)
+            Loc = sip.identify_gap(slide_bg_img, slide_block_img)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+            distance = 0.0
+            special_block = sip.check_special_block(slide_block_img)
+            if special_block:
+                distance = Loc[0] - 4
+            else:
+                distance = Loc[0] - 13
+            distance = distance / 350.946 * 371
+
+            self.move_slide(distance)
+
+            try:
+                while self.page.locator(".cff8").nth(0).get_by_text('元').inner_text(timeout=5000)[:-1] == "--":
+                    time.sleep(0.5)
+                amount = float(self.page.locator(".cff8").nth(0).get_by_text('元').inner_text()[:-1])
+                logging.info(f"今日电费：{amount}")
+            except:
+                self.page.screenshot(path='debug.png', full_page=True)
+
+                self.page.close()
+                self.context.close()
+                self.browser.close()
+                raise TimeoutError("验证或网络错误")
+
+            self.page.close()
+            self.context.close()
+            self.browser.close()
+
+            return amount
+
+    def loading_slide(self, timeout=10):
+        while True:
+            self.page.locator("canvas").nth(0).screenshot(path='slide.png')
+            if not sip.is_monochrome('slide.png'):
+                break
+            time.sleep(0.5)
+            logging.info("等待验证码图片加载...")
+
+            timeout -= 1
+            if timeout <= 0:
+                self.page.close()
+                self.context.close()
+                self.browser.close()
+                raise TimeoutError("加载验证码图片失败")
+
+    def move_slide(self, distance):
+        slider_btn_rect = self.page.locator('#slideVerify div').nth(3).bounding_box()
+        self.page.mouse.move(slider_btn_rect['x'] + 20, slider_btn_rect['y'] + 20)
+        time.sleep(0.5)
+        self.page.mouse.down()
+
+        tracks = sip.get_tracks(distance)
+
+        for x in tracks:
+            self.page.mouse.move(slider_btn_rect['x'] + 20 + x, random.randint(-5, 5) + slider_btn_rect['y'] + 20)
+        self.page.mouse.move(slider_btn_rect['x'] - 5 + tracks[-1] + 20,
+                             random.randint(-5, 5) + slider_btn_rect['y'] + 20)
+        self.page.mouse.move(slider_btn_rect['x'] + 5 + tracks[-1] + 20,
+                             random.randint(-5, 5) + slider_btn_rect['y'] + 20)
+        time.sleep(0.5)
+        self.page.mouse.up()
 
 
+# 在 run_task 函数中创建 ElectricityScraper 实例并调用 fetch_data 方法
 def run_task(data_fetcher: DataFetcher, sensor_updator: SensorUpdator):
     try:
-        user_id_list, balance_list, last_daily_date_list, last_daily_usage_list, yearly_charge_list, yearly_usage_list = data_fetcher.fetch()
-
-        for i in range(0, len(user_id_list)):
-            profix = f"_{user_id_list[i]}" if len(user_id_list) > 1 else ""
-            if balance_list[i] is not None:
-                sensor_updator.update(BALANCE_SENSOR_NAME + profix, None, balance_list[i], BALANCE_UNIT)
-            if last_daily_usage_list[i] is not None:
-                sensor_updator.update(DAILY_USAGE_SENSOR_NAME + profix, last_daily_date_list[i],
-                                      last_daily_usage_list[i], USAGE_UNIT)
-            if yearly_usage_list[i] is not None:
-                sensor_updator.update(YEARLY_USAGE_SENSOR_NAME + profix, None, yearly_usage_list[i], USAGE_UNIT)
-            if yearly_charge_list[i] is not None:
-                sensor_updator.update(YEARLY_CHARGE_SENESOR_NAME + profix, None, yearly_charge_list[i], BALANCE_UNIT)
-
-        logging.info("state-refresh task run successfully!")
+        # 其他代码...
+        electricity_scraper = ElectricityScraper(config)  # 创建 ElectricityScraper 实例
+        electricity_scraper.fetch_data()  # 调用 fetch_data 方法
+        # 其他代码...
     except Exception as e:
         logging.error(f"state-refresh task failed, reason is {e}")
         traceback.print_exc()
 
-
-def logger_init(level: str):
-    logger = logging.getLogger()
-    logger.setLevel(level)
-    logging.getLogger("urllib3").setLevel(logging.CRITICAL)
-    format = logging.Formatter("%(asctime)s  [%(levelname)-8s] ---- %(message)s", "%Y-%m-%d %H:%M:%S")
-    sh = logging.StreamHandler(stream=sys.stdout)
-    sh.setFormatter(format)
-    logger.addHandler(sh)
-
-
-if __name__ == "__main__":
-    main()
